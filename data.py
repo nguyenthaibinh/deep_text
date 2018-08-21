06f85a4bd6ff50942287ac338ae3ce41bfe8683c
binhnguyen <nguyenthaibinh@gmail.com>
2018-08-22 01:07:13 +1000

    changed

--- data.py
+++ data.py
@@ -5,6 +5,7 @@ QUORA_DATA_FILE = "./datasets/quora/quora_duplicate_questions.tsv"
 
 def load_quora_data():
 	df = pd.read_csv(QUORA_DATA_FILE, sep='\t')
+	df = df.dropna()
 	text1 = list(df['question1'])
 	text2 = list(df['question2'])
 	is_duplicate = list(df['is_duplicate'])
