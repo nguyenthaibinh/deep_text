e26450c18aa73d1d8ec44cbdcb0b1aa66f699746
binhnguyen <nguyenthaibinh@gmail.com>
2018-10-01 23:09:03 +0900

    changed

--- examples/quora_questions.py
+++ examples/quora_questions.py
@@ -26,7 +26,7 @@ from tensorboard_logger import configure, log_value
 QUORA_DIR = "../../datasets/pre/quora/"
 DATA_FILE = os.path.join(QUORA_DIR, "quora_duplicate_questions_preprocessed.tsv")
 VOCAB_FILE_1 = os.path.join(QUORA_DIR, "vocab1.csv")
-VOCAB_FILE_2 = os.path.join(QUORA_DIR, "vocab1.csv")
+VOCAB_FILE_2 = os.path.join(QUORA_DIR, "vocab2.csv")
 GLOVE_FILE = "./data/word_vectors/glove.840B.300d.txt"
 W2V_FILE = "./data/word_vectors/GoogleNews-vectors-negative300.bin"
 TEST_SPLIT = 0.1
